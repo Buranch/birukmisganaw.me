24aa3061a9b250d2a666a61e374901eabd7afffc
buranch <birukmisanaw@gmail.com>
2021-10-23 16:58:40 +0300

    Add gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "birukmiganaw.me",
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-sitemap"],
+};
