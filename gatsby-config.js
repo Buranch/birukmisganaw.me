5258745defc90fcfe675b861a3a1ab21cf98e057
buranch <birukmisanaw@gmail.com>
2021-10-24 11:17:25 +0300

    Remove unneeded contents from the site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.birukmisganaw.me",
     title: "birukmiganaw.me",
   },
   plugins: ["gatsby-plugin-sass", "gatsby-plugin-sitemap"],
